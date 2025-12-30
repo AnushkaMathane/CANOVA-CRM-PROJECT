85628ec0b5b59b31419cea10ae21d9d34bc18e65
AnushkaMathane <132830149+AnushkaMathane@users.noreply.github.com>
2025-12-30 17:03:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://canova-crm-project-chi.vercel.app/", "http://localhost:5174"],
     credentials: true,
   })
 );
